d79ce7dca8c61d8d5d85d2e58008fcdf06598165
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-21 20:25:53 -0300

    Add files via upload

---
+++ exercicio1_concatenacao_strings.py
@@ -0,0 +1,13 @@
+linguagem1 = 'Phyton '
+linguagem2 = ' C '
+linguagem3 = ' Java '
+linguagem4 = ' PHP'
+
+print('Linguagens de Programação:\n')
+
+print(linguagem1 + '-' * 5 + linguagem2 + '-' * 5 + linguagem3 + '-' * 5 + linguagem4)
+
+# Saída:
+#Linguagens de Programação:
+#
+#Phyton ----- C ----- Java ----- PHP
