15979377a9b5979ba05964e27022405d88e19378
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-21 21:11:04 -0300

    Create exercicio2_composicao_classica.py

---
+++ exercicio2_composicao_classica.py
@@ -0,0 +1,6 @@
+comida = 'Batata Frita'
+anoNascimento = 2006
+resultado = anoNascimento // 19
+s1 = 'Eu gosto de %s, nasci em %d e o divisão do meu ano de nascimento pela minha idade é %i' % (comida, anoNascimento, resultado)
+
+print(s1)
