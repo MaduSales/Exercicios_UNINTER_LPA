15b200114ae17aa8563f7349139d0ddf9f8fc463
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-21 21:13:05 -0300

    Create exercicio4_composicao_f-string.py

---
+++ exercicio4_composicao_f-string.py
@@ -0,0 +1,6 @@
+comida = 'Batata Frita'
+anoNascimento = 2006
+resultado = anoNascimento // 19
+s1 = f 'Eu gosto de {comida}, nasci em {anoNascimento} e o divisão do meu ano de nascimento pela minha idade é {resultado}'
+
+print(s1)
