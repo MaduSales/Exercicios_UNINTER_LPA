90f5aed42db8a3a5587ec39add25d9b73005add2
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-21 21:12:00 -0300

    Create exercicio3_composicao_moderna.py

---
+++ exercicio3_composicao_moderna.py
@@ -0,0 +1,6 @@
+comida = 'Batata Frita'
+anoNascimento = 2006
+resultado = anoNascimento // 19
+s1 = 'Eu gosto de {}, nasci em {} e o divisão do meu ano de nascimento pela minha idade é {}' .format(comida, anoNascimento, resultado)
+
+print(s1)
