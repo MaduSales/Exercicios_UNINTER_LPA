85f61f264dd776bdb7d0167fe18eea633d08575a
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-23 19:47:46 -0300

    Create exercicio3_funcao_length.py

---
+++ exercicio3_funcao_length.py
@@ -0,0 +1,7 @@
+# Crie uma variável de string que receba o seu nome completo.
+# Crie uma segunda variável booleana que receberá o resultado da comparação lógica que verifica se o tamanho do seu nome é menor ou igual ao valor 15. 
+# Imprima a variável booleana na tela.
+
+nome = 'Maria Eduarda de Sales Miranda'
+tamanho = len(nome) <= 15
+print(tamanho)
