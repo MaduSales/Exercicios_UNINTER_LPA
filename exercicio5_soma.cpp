44ad99635f73e87037dccfc0b94d29b7ae4ff92d
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-24 15:26:48 -0300

    Create exercicio5_soma.cpp

---
+++ exercicio5_soma.cpp
@@ -0,0 +1,14 @@
+# Composição Simples
+x = int(input('Digite um número: '))
+y = int(input('Insira outro número: '))
+
+c = x + y
+print('O resultado da soma é:' , c)
+
+
+# Composição Moderna
+x = int(input('Digite um número: '))
+y = int(input('Insira outro número: '))
+
+c = x + y
+print('O resultado da soma entre {} e {} é: {}' .format(x, y, c))
