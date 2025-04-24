ec0180ba12fbf4f6bc43a440e01a1726f8dfd8a6
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-04-23 22:03:47 -0300

    Add files via upload

---
+++ aula_pratica_python/main.py
@@ -0,0 +1,58 @@
+# Exercícios Algébricos
+
+#a)
+print('A somatória dos 5 primeiros números inteiros é: ', 1 + 2 + 3 + 4 + 5)
+
+#b)
+print('A média entre 23, 19 e 31 é: ', (23 + 19 + 31) // 3)
+
+#c)
+print('O número de vezes que 73 cabe em 403: ', 403 // 73)
+
+#d)
+print('A sobra quando 403 é dividido por 73: ', 403 % 73)
+
+#e)
+print('2 elevado à 10ª potência será: ', 2 ** 10)
+
+#f)
+print('O valor absoluto da diferença entre 54 e 57: ', abs(54 - 57))  #valor absoluto é o valor sem sinal
+
+#g)
+print('O menor valor entre 34, 29 e 31: ', min(34, 29, 31))
+
+
+# Exercícios de Atribuição
+
+#a) Atribuir o valor 3 à variável A
+A = 3
+print('Valor da variável A:' , A)
+
+#b) Atribuir o valor 4 à variável B
+B = 4
+print('Valor da variável B:' , B)
+
+#c) Atribuir à variável C o valor da expressão A*A + B*B
+C = A * A + B * B
+print('(A*A + B*B) é igual a: ' , C)
+
+
+#d)Faça as atribuições e, usando operadores + e * crie as saídas das variáveis
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+#1ª saída
+print(s1 + ' ' + s2 + ' ' + s3)
+
+#2ª saída
+print((s1 + ' ') * 10)
+
+#3ª saída
+print(s1 + ' '+ (s2 + ' ') * 2 + ' ' + (s3 + ' ') * 3)
+
+#4ª saída
+print(((s1 + ' ') + (s2 + ' ')) * 7)
+
+#5ª saída
+print(((s2 * 2) + s3 + ' ') * 5)
