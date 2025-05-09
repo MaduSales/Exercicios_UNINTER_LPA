f93d55f1f397431d137fb1f1fa7a74feae74eb21
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-05-09 10:20:44 -0300

    Create exercicio23_varredura_string.py

---
+++ exercicio23_varredura_string.py
@@ -0,0 +1,15 @@
+# Escreva um algoritmo que obtenha do usuário uma frase de tamanho entre 10 e 30 caracteres. Faça a impressão dela na tela da maneira exata como foi digitada e, em seguida, remova os espaços da frase e imprima novamente, sem espaços.
+frase = input('Digite uma frase de 10 a 30 caracteres: ')
+tamanho = len(frase)
+
+while tamanho < 10:
+  frase = input('Tente denovo, uma frase de 10 a 30 caracteres: ')
+  tamanho = len(frase)
+
+print(f'Frase com espaços: {frase}')
+print(f'Frase sem espaços: ', end="")
+
+
+for i in range(tamanho):
+  if frase[i] != " ":
+    print(frase[i], end='')
