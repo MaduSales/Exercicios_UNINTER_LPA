7226ef8fcc7bb78378882ece7668fa05cc734bb3
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-05-10 22:39:28 -0300

    Create funcao_com_parametros.py

---
+++ funcao_com_parametros.py
@@ -0,0 +1,9 @@
+# Passagem de parâmetros
+def realce(s1):
+  print('|','_' * 10,'|')
+  print('|','_' * 10,'|')
+  print(s1)
+  print('|','_' * 10,'|')
+  print('|','_' * 10,'|')
+
+realce('     MENU')
