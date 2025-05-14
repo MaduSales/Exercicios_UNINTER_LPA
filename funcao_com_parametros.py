85d61bf2c5e0d3f7482267247b19a0ede6eb9183
Maria Eduarda de Sales Miranda <166547195+MaduSales@users.noreply.github.com>
2025-05-14 11:45:09 -0300

    Delete funcao_com_parametros.py

--- funcao_com_parametros.py
+++
@@ -1,9 +0,0 @@
-# Passagem de parâmetros
-def realce(s1):
-  print('|','_' * 10,'|')
-  print('|','_' * 10,'|')
-  print(s1)
-  print('|','_' * 10,'|')
-  print('|','_' * 10,'|')
-
-realce('     MENU')
